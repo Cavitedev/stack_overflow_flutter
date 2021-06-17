c56748bf58c6c7ed5652444e96301489a67bddb6
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-06-17 17:07:32 +0200

    :sparkles: Answer FutureBuilder Question

--- lib/geolocator/lako_app.dart
+++ lib/geolocator/lako_app.dart
@@ -1,20 +1,19 @@
+//Question: https://stackoverflow.com/questions/68021398/trouble-initializing-a-position-variable-in-flutter-lateinitializationerror-f
+
 import 'package:flutter/material.dart';
 
 class LakoApp extends StatefulWidget {
-
-
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<LakoApp> {
-
   late int initialPosition;
 
   // @override
 
+  Future<int> getInitialPosition() async {
 
-  Future getInitialPosition <Position>() async {
-    initialPosition = await Future.delayed(Duration(seconds: 2), (){
+    initialPosition = await Future.delayed(Duration(seconds: 2), () {
       return 5;
     });
     return initialPosition;
@@ -28,11 +27,9 @@ class _MyAppState extends State<LakoApp> {
           ),
           actions: <Widget>[
             TextButton.icon(
-
               icon: Icon(Icons.person),
               label: Text('Mag Log-out'),
-              onPressed: () async {
-              },
+              onPressed: () async {},
             )
           ],
           backgroundColor: Colors.brown[700],
@@ -40,48 +37,44 @@ class _MyAppState extends State<LakoApp> {
         // bottomNavigationBar: BottomAppBar(
 
         persistentFooterButtons: [
-
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               TextButton(
-                //food and drinks button
+                  //food and drinks button
                   style: TextButton.styleFrom(
                       textStyle: TextStyle(fontSize: 20),
                       backgroundColor: Colors.brown,
                       primary: Colors.white),
-
-                  onPressed: () {}, child: Text('Nam at Nom')),
+                  onPressed: () {},
+                  child: Text('Nam at Nom')),
               TextButton(
-                //Items
+                  //Items
                   style: TextButton.styleFrom(
                       textStyle: TextStyle(fontSize: 20),
                       backgroundColor: Colors.brown,
                       primary: Colors.white),
+                  onPressed: () {},
+                  child: Text('Anik-anik')),
 
-                  onPressed: () {}, child: Text('Anik-anik')),
-
-              TextButton(
-                //Serbisyo
-                  style: TextButton.styleFrom(
-                      textStyle: TextStyle(fontSize: 20),
-                      backgroundColor: Colors.brown,
-                      primary: Colors.white),
-                  onPressed: () {}, child: Text('Serbisyo')),
+              // TextButton(
+              //   //Serbisyo
+              //     style: TextButton.styleFrom(
+              //         textStyle: TextStyle(fontSize: 20),
+              //         backgroundColor: Colors.brown,
+              //         primary: Colors.white),
+              //     onPressed: () {}, child: Text('Serbisyo')),
             ],
           ),
-
         ],
-        body:
-
-        FutureBuilder(
-
+        body: FutureBuilder<int>(
             future: getInitialPosition(),
             builder: (context, snapshot) {
-               return Text(initialPosition.toString());
-
-            }
-        )
-    );
+              if (snapshot.hasData) {
+                return Text(snapshot.data.toString());
+              }else{
+                return CircularProgressIndicator();
+              }
+            }));
   }
-  }
+}
